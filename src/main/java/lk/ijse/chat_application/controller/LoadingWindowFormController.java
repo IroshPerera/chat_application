7dff8a665842dad7a2199123eaab9512429571cd
IroshPerera <iroshlashan31@gmail.com>
2023-06-25 09:39:23 +0530

    modify Loading Window

--- src/main/java/lk/ijse/chat_application/controller/LoadingWindowFormController.java
+++ src/main/java/lk/ijse/chat_application/controller/LoadingWindowFormController.java
@@ -1,17 +1,27 @@
 package lk.ijse.chat_application.controller;
 
 import javafx.animation.ScaleTransition;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoadingWindowFormController implements Initializable {
     @FXML
     private ImageView imgLogo;
+    @FXML
+    private ProgressBar progressBar;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -21,5 +31,29 @@ public class LoadingWindowFormController implements Initializable {
         zoomIn.setToX(1.5);
         zoomIn.setToY(1.5);
         zoomIn.play();
+
+
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                for (int i = 0; i <= 100; i++) {
+                    updateProgress(i, 55);
+                    Thread.sleep(55);
+                }
+                return null;
+            }
+        };
+
+        progressBar.progressProperty().bind(task.progressProperty());
+        task.setOnSucceeded(event -> {
+
+
+
+                // Close the welcome stage
+                ((Stage) progressBar.getScene().getWindow()).close();
+
+        });
+        new Thread(task).start();
+
     }
 }
