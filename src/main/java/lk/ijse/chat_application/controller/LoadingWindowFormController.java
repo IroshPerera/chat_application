273dbbdaad200a9387c48c47812c6e5bac9f982a
IroshPerera <iroshlashan31@gmail.com>
2023-06-25 09:04:53 +0530

    add animation logo

--- src/main/java/lk/ijse/chat_application/controller/LoadingWindowFormController.java
+++ src/main/java/lk/ijse/chat_application/controller/LoadingWindowFormController.java
@@ -1,4 +1,25 @@
 package lk.ijse.chat_application.controller;
 
-public class LoadingWindowFormController {
+import javafx.animation.ScaleTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoadingWindowFormController implements Initializable {
+    @FXML
+    private ImageView imgLogo;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ScaleTransition zoomIn = new ScaleTransition(Duration.seconds(1.5), imgLogo);
+        zoomIn.setFromX(1.0);
+        zoomIn.setFromY(1.0);
+        zoomIn.setToX(1.5);
+        zoomIn.setToY(1.5);
+        zoomIn.play();
+    }
 }
