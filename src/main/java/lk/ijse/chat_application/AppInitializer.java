4fca8079415223978a9c510e9728f6d5c65fdec0
IroshPerera <iroshlashan31@gmail.com>
2023-06-25 08:45:55 +0530

    modify app initializer

--- src/main/java/lk/ijse/chat_application/AppInitializer.java
+++ src/main/java/lk/ijse/chat_application/AppInitializer.java
@@ -1,6 +1,9 @@
 package lk.ijse.chat_application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class AppInitializer extends Application {
@@ -11,6 +14,10 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/loading_window_form.fxml"))));
+        Image icon = new Image(getClass().getResourceAsStream("/view/img/logo.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle("WhisperWave");
+        stage.show();
     }
 }
