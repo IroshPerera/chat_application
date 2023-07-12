ff0062e8ad216b7fcc46a92882b4a3b6a1c95673
IroshPerera <iroshlashan31@gmail.com>
2023-07-12 20:51:50 +0530

    add user dto

---
+++ src/main/java/lk/ijse/chat_application/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.chat_application.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+    private String name;
+    private String password;
+}
