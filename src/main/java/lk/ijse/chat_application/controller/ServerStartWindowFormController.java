6a1f629479278e7ff42f8d479ce9c700c59e984b
IroshPerera <iroshlashan31@gmail.com>
2023-07-12 20:54:39 +0530

    few changes

--- src/main/java/lk/ijse/chat_application/controller/ServerStartWindowFormController.java
+++ src/main/java/lk/ijse/chat_application/controller/ServerStartWindowFormController.java
@@ -1,7 +1,6 @@
 package lk.ijse.chat_application.controller;
 
 import com.jfoenix.controls.JFXButton;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,13 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import lk.ijse.chat_application.dto.Server;
+import lk.ijse.chat_application.server.Server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class ServerStartWindowFormController {
     @FXML
@@ -39,7 +34,7 @@ public class ServerStartWindowFormController {
 
             root.getChildren().clear();
             Stage stage = (Stage) root.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login_window_from.fxml"))));
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/user_list_window_form.fxml"))));
             stage.show();
             stage.setOnCloseRequest(e-> {
                 System.exit(0);
